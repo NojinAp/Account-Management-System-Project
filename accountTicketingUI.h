412e15726e59d5a0e0748f73738f8cc430e26660
Nojin <nojinap@gmail.com>
2022-04-30 16:34:14 -0400

    Update accountTicketingUI.h

--- accountTicketingUI.h
+++ accountTicketingUI.h
@@ -2,9 +2,6 @@
     ==================================================
     Account Management System Project
     ==================================================
-    Name   : Nozhin Azarpanah
-    Date   : November 14, 2021
-
 */
 
 #ifndef ACCOUNTTICKETINGUI_H_
