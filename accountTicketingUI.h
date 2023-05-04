a9da76f87989e734034e7b9af39299b5bf6dda24
Nozhin Azarpanah <nojinap@gmail.com>
2023-05-04 14:47:47 -0400

    Update accountTicketingUI.h

--- accountTicketingUI.h
+++ accountTicketingUI.h
@@ -17,8 +17,6 @@ void applicationStartup(struct Account accounts[], int maxsize);
 int menuLogin(struct Account accounts[], int maxsize);
 //the main menu for an agent who has authorization to manage the accounts for the system
 void menuAgent(struct Account accounts[], int maxsize, struct Account* useraccount);
-//to search the array for a match on a desired account number
-int findAccountIndexByAcctNum(int accountNum, const struct Account accounts[], int maxsize, int prompt);
 //to display a detailed view of all the valid account records
 void displayAllAccountDetailRecords(const struct Account accounts[], int maxsize);
 //pause execution until user enters the enter key
